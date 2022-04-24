3cfdc0d88b9b33d1113871c88d60a351be45e444
Ion71229 <ionsimion279@gmail.com>
2022-04-24 11:57:56 +0000

    added category filtering

--- products/views.py
+++ products/views.py
@@ -9,19 +9,29 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    categories = None
 
-    if 'q' in request.GET:
-            query = request.GET['q']
-            if not query:
-                messages.error(request,
+    if request.GET:
+        if 'category' in request.GET:
+                categories = request.GET['category'].split(',')
+                products = products.filter(category__name__in=categories)
+                categories = Category.objects.filter(name__in=categories)
+
+
+
+        if 'q' in request.GET:
+                query = request.GET['q']
+                if not query:
+                    messages.error(request,
                                ("You didn't enter any search criteria!"))
-                return redirect(reverse('products'))
+                    return redirect(reverse('products'))
 
-            queries = Q(name__icontains=query)
-            products = products.filter(queries)
+                queries = Q(name__icontains=query)
+                products = products.filter(queries)
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
